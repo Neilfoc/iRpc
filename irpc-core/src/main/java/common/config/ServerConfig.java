661fe8fdc9bd57c168c8555a5cb87fc092170413
neilfoc <1319025936@qq.com>
2022-05-08 22:23:16 +0800

    服务端代理层

---
+++ irpc-core/src/main/java/common/config/ServerConfig.java
@@ -0,0 +1,14 @@
+package common.config;
+
+import lombok.Data;
+
+/**
+ * @author 11105157
+ * @Description
+ * @Date 2022/4/9
+ */
+@Data
+public class ServerConfig {
+
+    private Integer port;
+}
---
+++ irpc-core/src/main/java/common/constants/RpcConstants.java
@@ -0,0 +1,12 @@
+package common.constants;
+
+/**
+ * @author 11105157
+ * @Description
+ * @Date 2022/4/9
+ */
+public class RpcConstants {
+
+    // 魔数
+    public static final short MAGIC_NUMBER = 19812;
+}
